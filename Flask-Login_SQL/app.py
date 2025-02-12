cd5aaa5c77bf35e5e3b431dd5d83ebc351309f1c
Emily Medeiros <161541242+EmilyMedeiross@users.noreply.github.com>
2025-02-12 10:44:25 -0300

    Update app.py

--- Flask-Login_SQL/app.py
+++ Flask-Login_SQL/app.py
@@ -12,8 +12,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'SUPERMEGADIFICIL'
 login_manager.init_app(app)
 
-# quando precisar saber qual o usuario conectado
-# temos como consultar ele no banco
+#Consultar qual usuário está logado 
 @login_manager.user_loader
 def load_user(user_id):
     return User.get(user_id)
@@ -31,7 +30,6 @@ def gerar_hash(matricula, email, senha):
 def index():
     return render_template('index.html')
     
-
 @app.route('/login', methods=['POST', 'GET'])
 def login():
     if request.method == 'POST':
@@ -41,18 +39,14 @@ def login():
         user = User.get_by_matricula(matricula)
         hash = user.senha
 
-        if user and check_password_hash (hash, senha):
-            
+        if user and check_password_hash (hash, senha):  
             login_user(user)
-
             return redirect(url_for('dash'))
 
     return render_template('login.html')
 
-
 @app.route('/register', methods=['POST', 'GET'])
 def register():
-
     if request.method == 'POST':
         matricula = request.form['matricula']
         email = request.form['email']
@@ -62,15 +56,11 @@ def register():
 
     return render_template('register.html')
 
-
 @app.route('/dash')
 @login_required
 def dash():
-
     return render_template('dash.html')
 
-
-    
 @app.route('/logout', methods=['POST'])
 def logout():
     logout_user()
